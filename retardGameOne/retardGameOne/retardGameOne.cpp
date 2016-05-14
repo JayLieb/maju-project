46e7ebc89ccb55e4ccad0877422cd75a483d1fc7
Mattheus van Iterson <sleepymaat@gmx.de>
2016-05-14 16:54:47 +0200

    World print

--- retardGameOne/retardGameOne/retardGameOne.cpp
+++ retardGameOne/retardGameOne/retardGameOne.cpp
@@ -2,30 +2,20 @@
 //
 
 #include "stdafx.h"
+#include "World.h"
 #include <iostream>
 
  using namespace std;
-
-
- void drawField(int x, int y)
- {
-	 for (int i = 0; i < y; i++)
-	 {
-		 for (int j = 0; j < x; j++)
-		 {
-			 cout << "0";
-		 }
-		 cout << "\n";
-	 }
- }
-
-
+ 
 //Gameloop
 int main()
 {
 	cout << "fick dich du fotze\n";
 
-	drawField(10, 10);
+	World* myWorld = new World();
+	myWorld->print();
+	delete myWorld;
+
 	cin.clear();
 	cin.ignore(cin.rdbuf()->in_avail());
 	cin.get();
