5ed240480b3c3e3a730d07eddf25e8917e390884
Juri <juri.lieb@gmx.de>
2016-05-14 14:32:04 +0200

    Erste Datei

---
+++ retardGameOne/retardGameOne/retardGameOne.cpp
@@ -0,0 +1,19 @@
+// retardGameOne.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+ using namespace std;
+
+
+int main()
+{
+	cout << "fick dich du fotze";
+
+	cin.clear();
+	cin.ignore(cin.rdbuf()->in_avail());
+	cin.get();
+    return 0;
+}
+
