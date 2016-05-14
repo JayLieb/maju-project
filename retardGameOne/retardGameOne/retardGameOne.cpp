6c57084e0ced5ce3579e0268f90c2b0b607078ec
Mattheus van Iterson <sleepymaat@gmx.de>
2016-05-14 14:50:31 +0200

    drawField

--- retardGameOne/retardGameOne/retardGameOne.cpp
+++ retardGameOne/retardGameOne/retardGameOne.cpp
@@ -7,13 +7,26 @@
  using namespace std;
 
 
+ void drawField(int x, int y)
+ {
+	 for (int i = 0; i < y; i++)
+	 {
+		 for (int j = 0; j < x; j++)
+		 {
+			 cout << "0";
+		 }
+		 cout << "\n";
+	 }
+ }
+
+
 int main()
 {
-	cout << "fick dich du fotze";
+	cout << "fick dich du fotze\n";
 
+	drawField(10, 10);
 	cin.clear();
 	cin.ignore(cin.rdbuf()->in_avail());
 	cin.get();
     return 0;
 }
-
